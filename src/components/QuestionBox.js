676e385c4d8a2f351fdc35784a72bef3dd58948c
Nanosync <10258208+Nanosync@users.noreply.github.com>
2020-01-18 12:29:45 +0800

    Formatting changes

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
-const QuestionBox = ({question, options, selected}) => {
+const QuestionBox = ({ question, options, selected }) => {
     //setAnswer: function to update answer variable
     const [answer, setAnswer] = useState(options);
     //index from answer array
     return (
-        <div className= "questionBox">
+        <div className="questionBox">
             <div className="question">{question}</div>
             {answer.map((text, index) => (
                 <button
-                    key = {index}
+                    key={index}
                     className="answerBtn"
                     onClick={() => {
                         //rewrite array with chosen answer
--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ class QuizBee extends Component {
         this.getQuestions();
         //condition fails during render
         this.setState({
-           score: 0,
-           responses: 0
+            score: 0,
+            responses: 0
         });
     };
     componentDidMount() {
@@ -42,26 +42,26 @@ class QuizBee extends Component {
     render() {
         return (
             <div className="container">
-                <div className="title"> QuizBee </div>
+                <div className="title">QuizBee</div>
                 {this.state.questionBank.length > 0
-                && this.state.responses < 5
-                && this.state.questionBank.map(
-                    ({question , answers, correct, questionId}) => (
-                        <QuestionBox
-                            question = {question}
-                            options = {answers}
-                            key = {questionId}
-                            selected = {answer => this.computeAnswer(answer, correct)}
-                        />
-                    )
-                )}
+                    && this.state.responses < 5
+                    && this.state.questionBank.map(
+                        ({ question, answers, correct, questionId }) => (
+                            <QuestionBox
+                                question={question}
+                                options={answers}
+                                key={questionId}
+                                selected={answer => this.computeAnswer(answer, correct)}
+                            />
+                        )
+                    )}
 
                 {this.state.responses === 5 ? (
-                    <Result score = {this.state.score} playAgain={this.playAgain}/>
-                    ) : null}
+                    <Result score={this.state.score} playAgain={this.playAgain} />
+                ) : null}
             </div>
         );
     }
 }
 
-ReactDOM.render(<QuizBee/>, document.getElementById("root"));
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
