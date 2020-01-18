83aba2bfc65f87e4979861de4b3fff7a24157bf6
Nanosync <10258208+Nanosync@users.noreply.github.com>
2020-01-18 13:39:18 +0800

    Added settings

---
+++ src/components/Settings/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import DEFAULT_GAME_SETTINGS from '../services/settingsService';
+
+class Settings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            game: DEFAULT_GAME_SETTINGS
+        };
+    }
+    render() {
+        const { numberOfQuestions, difficulty, category } = this.state.game;
+        return (
+            <div>
+                <p>Number of Questions: {numberOfQuestions}</p>
+                <p>Difficulty: {difficulty}</p>
+                <p>Category: {category}</p>
+            </div>
+        );
+    }
+}
+
+export default Settings;
---
+++ src/services/Settings/index.js
@@ -0,0 +1,7 @@
+const DEFAULT_GAME_SETTINGS = {
+    numberOfQuestions: 50,
+    difficulty: "easy",
+    category: "all"
+};
+
+export default DEFAULT_GAME_SETTINGS;
