2b011440822fe2f66cac3788e1c5ca9351d79d1e
Nanosync <10258208+Nanosync@users.noreply.github.com>
2020-01-27 16:40:08 +0800

    Updates

---
+++ precache-manifest.12722f0ab098a12ce25217ad9a7b57d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22117817c148378f2882e09faeb55e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01652d9f7ecc521161a8",
+    "url": "/static/css/main.4f3e0f06.chunk.css"
+  },
+  {
+    "revision": "07445281520533bb9849",
+    "url": "/static/js/2.8b90a811.chunk.js"
+  },
+  {
+    "revision": "f2bdd63ea1a7b9a1a8d52deeb08892b6",
+    "url": "/static/js/2.8b90a811.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01652d9f7ecc521161a8",
+    "url": "/static/js/main.998c3424.chunk.js"
+  },
+  {
+    "revision": "551b621f871d16eb5066",
+    "url": "/static/js/runtime-main.be9fec38.js"
+  }
+]);
